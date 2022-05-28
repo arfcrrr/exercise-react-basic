7dfcca0c6ae818a89d69e40f6031e2b88090ffaf
arfcrrr <ajengarifa.char@gmail.com>
2022-05-28 11:59:09 +0700

    [ADD] Google OAuth Login

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="93227781019-0b9ml7rn0cns4cbqeo68a86heqanoeek.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>;
   </React.StrictMode>
 );
 
